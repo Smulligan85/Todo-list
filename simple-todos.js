dfc3d5c25b14a6fce82b72381e7fa5114287ae45
Sean Mulligan <sean.mulligan85@gmail.com>
2015-06-21 15:50:13 -0700

    Initial commit

---
+++ simple-todos.js
@@ -0,0 +1,29 @@
+Tasks = new Mongo.Collection("tasks");
+
+if (Meteor.isClient) {
+  // This code only runs on the client
+  Template.body.helpers({
+    tasks: function () {
+      return Tasks.find({});
+    }
+  });
+
+  Template.body.events({
+    "submit .new-task": function (event) {
+      var text = event.target.text.value;
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()
+      })
+      event.target.text.value = "";
+
+      return false;
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
